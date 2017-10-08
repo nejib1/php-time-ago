64bf3517999230e9e62ee01d315b59d93e4b77a7
nejib1 <nejib.ben-ahmed-partner@aperam.com>
2017-10-08 21:40:32 +0200

    Create pt.php

---
+++ translations/pt.php
@@ -0,0 +1,16 @@
+<?php
+
+$timeAgoStrings = array(
+"aboutOneDay"      =>  "1 dia atrás",
+"aboutOneHour"     =>  "cerca de 1 hora atrás",
+"aboutOneMonth"    =>  "por volta de 1 mês atrás",
+"aboutOneYear"     =>  "cerca de 1 ano atrás",
+"days"             =>  "%s dias atrás",
+"hours"            =>  "%s horas atrás",
+"lessThanAMinute"  =>  "a menos de um minuto atras",
+"lessThanOneHour"  =>  "%s minutos atrás",
+"months"           =>  "%s meses ag",
+"oneMinute"        =>  "1 minuto atrás",
+"years"            =>  "mais de %s anos atrás",
+'never'            => "Never"
+);
