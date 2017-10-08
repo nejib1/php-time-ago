991cd116341cc5ba85c276ce04350f6beeb81da1
nejib1 <nejib.ben-ahmed-partner@aperam.com>
2017-10-08 21:44:01 +0200

    Update fr.php
    Hello, this is the better translation to fr (I am french..)

--- translations/fr.php
+++ translations/fr.php
@@ -1,15 +1,16 @@
 <?php
 
 $timeAgoStrings = array(
-  'aboutOneDay' => "depuis un jour",
-  'aboutOneHour' => "depuis à peu près une heure",
-  'aboutOneMonth' => "depuis à peu près un mois",
-  'aboutOneYear' => "depuis à peu près une année",
-  'days' => "depuis %s jours",
-  'hours' => "depuis %s heures",
-  'lessThanAMinute' => "depuis moins d'une minute",
-  'lessThanOneHour' => "depuis %s minutes",
-  'months' => "depuis %s mois",
-  'oneMinute' => "depuis une minute",
-  'years' => "depuis plus de %s années"
+"aboutOneDay"      => "il y a 1 jour",
+"aboutOneHour"     => "Il ya environ 1 heure",
+"aboutOneMonth"    => "il y a 1 mois environ",
+"aboutOneYear"     => "il y a environ 1 année",
+"days"             => "Il y a %s jours",
+"hours"            => "Il y a %s heures",
+"lessThanAMinute"  => "il y a moins d'une minute",
+"lessThanOneHour"  => "Il y a %s minutes",
+"months"           => "Il y a %s mois",
+"oneMinute"        => "il y a 1 minute",
+"years"            => "il y a plus de %s années",
+'never'            => "Never"
 );
